e3bb058b00e9219d7395c9c0120c27fb2902852d
darwinmero2510-creator <darwinmero2510@gmail.com>
2026-01-28 11:49:50 -0500

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -3,9 +3,10 @@
 $host = getenv('DB_HOST') ?: "sql300.infinityfree.com"; 
 $user = getenv('DB_USER') ?: "if0_41000446";           
 $pass = getenv('DB_PASS') ?: "uhLVwIB9ZWNug1";       
-$db   = getenv('DB_NAME') ?: "if0_41000446_web";       
+$db   = getenv('DB_NAME') ?: "if0_41000446_web";   
+$port = getenv('DB_PORT') ?: "24998";
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
+$conexion = mysqli_connect($host, $user, $pass, $db, $port);
 mysqli_set_charset($conexion, "utf8");
 
 if (!$conexion) {
